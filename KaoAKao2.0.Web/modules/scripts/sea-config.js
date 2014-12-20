2c0c42e9610dbcace7e04a8d784e37a7ce27e8f8
sunallen521 <sunallen@163.com>
2014-12-20 22:25:32 +0800

    后台模版框架

---
+++ KaoAKao2.0.Web/modules/scripts/sea-config.js
@@ -0,0 +1,30 @@
+﻿
+/*基础配置*/
+seajs.config({
+    base: "/modules/",
+    alias: {
+        "jquery": "scripts/jquery-1.11.1.js",
+        "jqueryui": "scripts/jquery-ui-1.8.20.js",
+        "global": "scripts/global.js"        
+    }
+});
+
+seajs.config({
+    alias: {
+        //弹出层插件
+        "easydialog": "plug/easydialog/easydialog.js",
+        //验证插件
+        "verify": "plug/verify.js",
+        //分页插件
+        "paginate": "plug/datapager/paginate.js",
+        //doT
+        "dot": "plug/doT.js"
+    }
+});
+
+seajs.config({
+    alias: {
+        "easydialog-css": "plug/easydialog/easydialog.css"
+    }
+});
+
