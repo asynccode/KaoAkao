3a2b65d0ade3a7097659aae4fc0bfb80817c828f
sunallen521 <sunallen@163.com>
2015-01-04 23:37:18 +0800

    课程章节的添加、编辑、删除

--- KaoAKao.DAL/CourseDAL.cs
+++ KaoAKao.DAL/CourseDAL.cs
@@ -37,6 +37,30 @@ namespace KaoAKao.DAL
             return GetDataTable("select c.*,cc.PID,cc.CategoryName CName from Courses c join CourseCategory cc on c.CategoryID=cc.CategoryID where CourseID=@CourseID", paras, CommandType.Text);
         }
 
+        public DataTable GetCourseLessons(string courseid)
+        {
+            SqlParameter[] paras = { 
+                                       new SqlParameter("@CourseID",courseid)
+                                   };
+            return GetDataTable("select * from Lessons where Status<>9  and CourseID=@CourseID order by Sort,CreateDate", paras, CommandType.Text);
+        }
+
+        public DataTable GetCourseLessons(string courseid, string pid)
+        {
+            SqlParameter[] paras = { 
+                                       new SqlParameter("@CourseID",courseid),
+                                       new SqlParameter("@PID",pid)
+                                   };
+            return GetDataTable("select * from Lessons where Status<>9 and CourseID=@CourseID and PID=@PID  order by Sort,CreateDate", paras, CommandType.Text);
+        }
+
+        public DataTable GetCourseLessonByID(string lessonid)
+        {
+            SqlParameter[] paras = { 
+                                       new SqlParameter("@LessonID",lessonid)
+                                   };
+            return GetDataTable("select * from Lessons where LessonID=@LessonID", paras, CommandType.Text);
+        }
         #endregion
 
         #region 添加
@@ -75,6 +99,29 @@ namespace KaoAKao.DAL
                                        new SqlParameter("@LimitLevel",limitLevel),
                                        new SqlParameter("@KeyWords",keyWords),
                                        new SqlParameter("@Description",desc),
+                                       new SqlParameter("@OperateIP",operateIP),
+                                       new SqlParameter("@OperateID",operateID)
+                                   };
+            return ExecuteScalar(sql, paras, CommandType.Text);
+        }
+
+        public object AddCourseLesson(string lessonName, string courseID, string pid,  string keyWords, string description, string videoURL, string videoSize, int sort, string operateIP, string operateID)
+        {
+            string sql = @" declare @LessonID nvarchar(64) set @LessonID=NEWID() 
+                            INSERT INTO Lessons(LessonID,LessonName,CourseID,PID,Status,RadioURL,RadioSize,ViewCount,PraiseCount,ShareCount,CollectCount,Sort,IsHot,CreateDate
+                                                 ,LastOperateDate,Keywords,Description,OperateIP,OperateID)
+                            values (@LessonID,@LessonName,@CourseID,@PID,1,@RadioURL,@RadioSize,0,0,0,0,@Sort,1,getdate(),getdate(),@Keywords,@Description,@OperateIP,@OperateID)
+                            select @LessonID";
+
+            SqlParameter[] paras = { 
+                                       new SqlParameter("@LessonName",lessonName),
+                                       new SqlParameter("@CourseID",courseID),
+                                       new SqlParameter("@PID",pid),
+                                       new SqlParameter("@KeyWords",keyWords),
+                                       new SqlParameter("@Description",description),
+                                       new SqlParameter("@RadioURL",videoURL),
+                                       new SqlParameter("@RadioSize",videoSize),
+                                       new SqlParameter("@Sort",sort),
                                        new SqlParameter("@OperateIP",operateIP),
                                        new SqlParameter("@OperateID",operateID)
                                    };
@@ -125,6 +172,27 @@ namespace KaoAKao.DAL
             return ExecuteNonQuery(sql, paras, CommandType.Text) > 0;
         }
 
+        public bool EditCourseLesson(string lessonID, string lessonName, string courseID, string pid, string keyWords, string description, string videoURL, string videoSize, int sort, string operateIP, string operateID)
+        {
+            string sql = @" update Lessons set LessonName=@LessonName,PID=@PID,KeyWords=@KeyWords,
+                            Description=@Description ,RadioURL=@VideoURL,RadioSize=@VideoSize,Sort=@Sort,LastOperateDate=GETDATE(),OperateIP=@OperateIP,OperateID=@OperateID
+                            where LessonID=@LessonID";
+
+            SqlParameter[] paras = { 
+                                       new SqlParameter("@LessonID",lessonID),
+                                       new SqlParameter("@LessonName",lessonName),
+                                       new SqlParameter("@PID",pid),
+                                       new SqlParameter("@KeyWords",keyWords),
+                                       new SqlParameter("@Description",description),
+                                       new SqlParameter("@VideoURL",videoURL),
+                                       new SqlParameter("@VideoSize",videoSize),
+                                       new SqlParameter("@Sort",sort),
+                                       new SqlParameter("@OperateIP",operateIP),
+                                       new SqlParameter("@OperateID",operateID)
+                                   };
+            return ExecuteNonQuery(sql, paras, CommandType.Text) > 0;
+        }
+
         #endregion
 
         #region 删除
@@ -147,6 +215,18 @@ namespace KaoAKao.DAL
                            where CourseID=@CourseID ";
             SqlParameter[] paras = { 
                                        new SqlParameter("@CourseID",courseid),
+                                       new SqlParameter("@OperateIP",operateIP),
+                                       new SqlParameter("@OperateID",operateID)
+                                   };
+            return ExecuteNonQuery(sql, paras, CommandType.Text) > 0;
+        }
+
+        public bool DeleteCourseLessons(string lessonid, string operateIP, string operateID)
+        {
+            string sql = @" update  Lessons set [Status] = 9,LastOperateDate=GETDATE(),OperateIP=@OperateIP,OperateID=@OperateID 
+                            where LessonID=@LessonID";
+            SqlParameter[] paras = { 
+                                       new SqlParameter("@LessonID",lessonid),
                                        new SqlParameter("@OperateIP",operateIP),
                                        new SqlParameter("@OperateID",operateID)
                                    };
---
+++ KaoAKao2.0.Web/modules/manage/scripts/course-lesson.js
@@ -0,0 +1,80 @@
+﻿/*
+    课程章节管理JS文件
+*/
+define(function (require, exports, module) {
+    var $ = require("jquery"),
+        Verify = require('verify'), VerifyObject,
+        Global = require("global"),
+        doT = require("dot");
+    var Params = {
+        CourseID: ""
+    };
+    var CourseLesson = {
+        //新建页JS
+        initCreate: function (id) {
+            Params.CourseID = id;
+            this.bindCreateEvent();
+        },
+        //绑定事件
+        bindCreateEvent: function () {
+            var _self = this;
+            VerifyObject = Verify.createVerify({
+                element: ".verify",
+                emptyAttr: "data-empty",
+                verifyReg: "data-type",
+                regText: "data-text"
+            });
+            $("#btnSaveLesson").click(function () {
+                if (!VerifyObject.isPass())
+                    return;
+                _self.savaLesson();
+            });
+        },
+        //保存分类
+        savaLesson : function () {
+            var _self = this;
+            var Lesson = {
+                LessonID: _self.LessonID,
+                LessonName: $("#lessonName").val().trim(),
+                CourseID: Params.CourseID,
+                PID: $("#pLesson").val(),
+                RadioURL: $("#videoURL").val(),
+                RadioSize: $("#videoSize").val(),
+                Sort: $("#sort").val(),
+                Keywords: $("#lessonKeyWords").val(),
+                Description: $("#description").val()
+            };
+
+            Global.post("/Manage/Course/SaveCourseLesson", { lesson: JSON.stringify(Lesson) }, function (data) {
+                if (data.ID.length > 0) {
+                    location.href = "/Manage/Course/Lessons/" + Params.CourseID;
+                }
+            });
+
+        },
+
+        //详情页JS
+        initDetail: function (id, courseid ,callBack) {
+            this.LessonID = id;
+            Params.CourseID = courseid;
+            this.bindCreateEvent();
+            !!callBack && callBack();
+            //this.bindLesson(id, callBack);
+        },
+        //绑定页面信息
+        //bindLesson: function (id, callBack) {
+        //    Global.post("/Manage/Courses/GetCourseCategoryByID", { cid: id }, function (data) {
+        //        var model = data.Item;
+        //        $("#categoryName").val(model.CategoryName);
+        //        $("#pcategory").val(model.PID);
+        //        $("#categoryImage").attr("src", model.CategoryImg);
+        //        $("#categoryKeyWords").val(model.KeyWords);
+        //        $("#description").val(model.Description);
+                
+        //    });
+        //}
+    };
+
+
+    module.exports = CourseLesson;
+})
