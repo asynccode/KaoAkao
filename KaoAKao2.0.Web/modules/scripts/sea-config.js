60ad9e36bace2c5bee799a1640ffc36492302930
muge <xiaoxiaomuge@163.com>
2015-01-04 23:15:43 +0800

    课程页面

--- KaoAKao2.0.Web/modules/scripts/sea-config.js
+++ KaoAKao2.0.Web/modules/scripts/sea-config.js
@@ -3,7 +3,7 @@
 seajs.config({
     base: "/modules/",
     alias: {
-        "jquery": "scripts/jquery-1.11.1.js",
+        "jquery": "scripts/jquery-1.7.2.js",
         "jqueryui": "scripts/jquery-ui-1.8.20.js",
         "global": "scripts/global.js"        
     }
