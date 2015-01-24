65df32e02e5fb72bb6e0fec3fb9b999d6bf80079
muge <xiaoxiaomuge@163.com>
2015-01-24 23:38:34 +0800

    新增 用户中心

---
+++ KaoAKao2.0.Web/Controllers/UserCenterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KaoAKao2._0.Web.Controllers
+{
+    public class UserCenterController : Controller
+    {
+        //
+        // GET: /UserCenter/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
